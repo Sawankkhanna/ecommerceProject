1923ab7c14397742993ae67af702a9dcdf3c9fed
charan1925 <nawabmakan@gmail.com>
2023-06-02 19:12:27 -0400

    Create App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>This is an Ecommerce project</h1>
+      <h2>This is charandeep</h2>
+      <h3>This is Rehan</h3>
+      <h4>RThis is anshika</h4>
+    </>
+  );
+}
+export default App;
