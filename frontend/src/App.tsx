c38bf9015ad47724e2874e88be0da7f367b8dfd3
ANSHIKA199925 <anshika252825@gmail.com>
2023-06-05 15:10:54 -0400

    Update App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>This is an Ecommerce project</h1>
+      <h2>This is charandeep</h2>
+
+      <h3>This is Rehan</h3>
+      <h4>RThis is anshika</h4>
+      <h1>Hello </h1>
+    </>
+  );
+}
+export default App;
